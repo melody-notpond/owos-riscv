cda9ba7e6813fc3e36c095c01b1696ae93b13766
jenra <a>
2021-04-30 18:52:32 -0400

    Finished block init code

--- src/root/kernel/kernel.c
+++ src/root/kernel/kernel.c
@@ -1,10 +1,16 @@
 #include "uart.h"
 #include "virtio.h"
 
+void kmain() {
+    while (1);
+}
+
 void kinit() {
     uart_puts("Booted into kernel\n");
 
     virtio_probe();
+    uart_puts("Finished initialisation.\n");
 
-    while (1);
+    kmain();
 }
+
